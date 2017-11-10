5d888677a886e8616d78e5121f74444ab600b8aa
Skelyos <pat123john123@gmail.com>
2017-11-10 14:59:12 +0000

    Add project files.

---
+++ Task-Check/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("hellow world");
+                System.Threading.Thread.Sleep(1000 * 60 * 5); // Sleep for 5 minutes
+            }
+        }
+    }
+}
