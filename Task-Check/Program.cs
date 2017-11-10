d4481ceea821a4b947b197bfafff733bee6bfe1d
Skelyos <pat123john123@gmail.com>
2017-11-10 15:56:32 +0000

    Added in a streamreader and streamwritter

--- Task-Check/Program.cs
+++ Task-Check/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task_Check
 {
@@ -20,7 +21,29 @@ namespace Task_Check
                 var Minute = DateTime.UtcNow.Minute;
                 var Second = DateTime.UtcNow.Second;
 
-                Console.WriteLine("The time is = " + Day + "/" + Month + "/" + Year + " @ " + Hour + ":" + Minute + ":" + Second);
+                var mod = Second % 10;
+
+                string Location = System.Configuration.ConfigurationManager.AppSettings["LogPath"];
+                string Destination = System.Configuration.ConfigurationManager.AppSettings["LogDestination"];
+
+                if (mod == 0)
+                {
+                    using (StreamReader file1 = new StreamReader(Location + ".txt", true))
+                    {
+
+                    }
+
+                    using (StreamWriter file2 = new StreamWriter(Destination + ".txt", true))
+                    {
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The time is = " + Day + "/" + Month + "/" + Year + " @ " + Hour + ":" + Minute + ":" + Second);
+                }
+
+
 
                 System.Threading.Thread.Sleep(1000 * 1); // Sleep for x time
             }
